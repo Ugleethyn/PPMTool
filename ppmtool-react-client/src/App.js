99e5e4539d7fc91b36a0553065eff14ed5dc23fc
Ugleethyn <tasoskladis@hotmail.gr>
2021-06-14 16:16:22 +0300

    React- save Token at LocalStorage/ Invalid Token error

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -13,6 +13,26 @@ import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProj
 import Landing from "./components/Layout/Landing";
 import Login from "./components/UserManagment/Login";
 import Register from "./components/UserManagment/Register";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./SecurityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken,
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    //window.location.href = "/";
+  }
+}
 
 function App() {
   return (
